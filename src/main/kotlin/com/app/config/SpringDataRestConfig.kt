72ee6f1e2c6c7b8a2cc46761b836f35419419697
William Chen <william_w_chen@trendmicro.com>
2025-04-16 15:49:08 +0800

    feat: Enhance Spring Data REST configuration for entity exposure
    - Add new entities (BaseCompany, ArchitectureFirm, MaterialSupplier, DesignerProfile) to the exposed IDs configuration.
    - Disable default POST method exposed by SpringDataRest

--- src/main/kotlin/com/app/config/SpringDataRestConfig.kt
+++ src/main/kotlin/com/app/config/SpringDataRestConfig.kt
@@ -1,12 +1,17 @@
 package com.app.config
 
-import com.app.repository.common.EntityValues
-import com.app.repository.common.ValidationPolicy
+import com.app.repository.common.*
+import com.app.repository.identity.company.ArchitectureFirm
+import com.app.repository.identity.company.BaseCompany
+import com.app.repository.identity.company.MaterialSupplier
+import com.app.repository.identity.individual.DesignerProfile
+import com.app.service.mapper.MaterialMapper
 import com.security.repository.User
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer
+import org.springframework.http.HttpMethod
 
 
 @Configuration
@@ -16,10 +21,22 @@ class SpringDataRestConfig {
     fun repositoryRestConfigurer(): RepositoryRestConfigurer {
         return RepositoryRestConfigurer.withConfig { config: RepositoryRestConfiguration ->
             config.exposeIdsFor(
+                Product::class.java,
+                ProductCategory::class.java,
+                ProductAssociation::class.java,
+                ProductCategoryAssociation::class.java,
                 User::class.java,
                 EntityValues::class.java,
-                ValidationPolicy::class.java
-            )
+                ValidationPolicy::class.java,
+
+                BaseCompany::class.java,
+                ArchitectureFirm::class.java,
+                MaterialSupplier::class.java,
+                DesignerProfile::class.java
+            ).exposureConfiguration.forDomainType(Product::class.java)
+                .withItemExposure { metadata, httpMethods -> httpMethods.disable(HttpMethod.POST) }
+                .withCollectionExposure { metadata, httpMethods -> httpMethods.disable(HttpMethod.POST) }
+
         }
     }
 }
