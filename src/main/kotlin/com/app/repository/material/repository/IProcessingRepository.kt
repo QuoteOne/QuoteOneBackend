cddfb1b4f7e3bc4ff90044f8d16874d2f6b2425c
William Chen <william_w_chen@trendmicro.com>
2025-04-18 15:37:53 +0800

    refactor: Remove IProcessingRepository and Processing model from the project

--- src/main/kotlin/com/app/repository/material/repository/IProcessingRepository.kt
+++
@@ -1,7 +0,0 @@
-//package com.app.repository.material.repository
-//
-//import com.app.repository.material.repository.models.Processing
-//import org.springframework.data.jpa.repository.JpaRepository
-//import java.util.UUID
-//
-//interface IProcessingRepository: JpaRepository<Processing, UUID>
